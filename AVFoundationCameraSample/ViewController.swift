504e4120c655e2d0a57aa2d27b05e7000a228079
Takahito Murakami <me@murakamitakahito.com>
2019-09-04 21:04:21 +0900

    Initial Commit

---
+++ AVFoundationCameraSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundationCameraSample
+//
+//  Created by Takahito Murakami on 2019/09/04.
+//  Copyright © 2019 Takahito Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
